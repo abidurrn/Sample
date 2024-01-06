f02b4e8b2177e7e6593b58a9f74a440f90521b32
Abidur <abidur.rn@gmail.com>
2024-01-06 12:31:24 +0300

    06012024

---
+++ class/main.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Form design</title>
+
+
+
+
+
+
+
+
+    </head>
+    <body>
+        <div>
+            <div>
+                <div>
+                    1. Full Name: <input type="text" name="fullname" placeholder="Enter your name">
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+        <!--    1. Full Name:
+                2. Email:
+                3. Date of Birth:
+                4. Phone Number:
+                5. Nationality: 
+                6. Gender:
+                7. Address:
+                8. Message:
+                9. Want: Printed Book, eBook, Audiobook
+                10. Interest: Science, Emotion, History, Climate, Politics, Food, Travel, Romance, Horror
+
+                I accept the terms and conditions
+                submit
+
+        -->
+
+    </body>
+</html>
